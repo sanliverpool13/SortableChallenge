bb66ab4f8a8900cdf975fe371550184aab4c6705
sanliverpool13 <kairosh.sanjar@gmail.com>
2020-10-18 23:41:40 -0400

    added Auction class

---
+++ Auction.js
@@ -0,0 +1,46 @@
+class Auction {
+  constructor(site, units, bids) {
+    this.site = site;
+    this.bids = bids;
+    this.unitBidMap = {};
+    this.initUnitBidMap(units);
+    this.finalBids = [];
+    this.validBids = 0;
+  }
+
+  initUnitBidMap(units) {
+    units.forEach((unit) => {
+      this.unitBidMap[unit] = {};
+    });
+  }
+
+  isUnitInvalid(unit) {
+    return !this.unitBidMap[unit];
+  }
+
+  isFloor(bid, floor, bidder) {
+    let adjustedAmount = bid["bid"] * (1 + bidder.adjustment);
+    return adjustedAmount >= floor;
+  }
+
+  setMaxUnitBid(bid, bidder) {
+    this.validBids++;
+    let currentBidForUnit = this.unitBidMap[bid["unit"]];
+    if (currentBidForUnit.length === 0) {
+      this.unitBidMap[bid["unit"]] = bid;
+    } else {
+      let adjustedAmount = bid["bid"] * (1 + bidder.adjustment);
+      if (adjustedAmount > currentBidForUnit["bid"]) {
+        this.unitBidMap[bid["unit"]] = bid;
+      }
+    }
+  }
+
+  setFinalBids() {
+    if (this.validBids === 0) {
+      this.finalBids = [];
+    } else {
+      this.finalBids = Object.values(this.unitBidMap);
+    }
+  }
+}
